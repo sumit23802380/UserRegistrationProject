f119298f47ecc38571f62bc08a44cf0f68c6cd07
sumit23802380 <sumitjangid1692153@gmail.com>
2023-12-01 20:09:00 +0530

    Added : UC1 FirstName Validation

--- src/main/java/com/bridgelabz/userregistration/User.java
+++ src/main/java/com/bridgelabz/userregistration/User.java
@@ -1,5 +1,25 @@
 package com.bridgelabz.userregistration;
 
-public class User {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class User {
+    private String firstName;
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public boolean setFirstName(String firstName){
+        if(validateFirstName(firstName)){
+            this.firstName = firstName;
+            return true;
+        }
+        System.out.println("Please provide at least 3 character , with first letter Capital " + firstName);
+        return false;
+    }
+    private boolean validateFirstName(String firstName){
+        String regex = "^[A-Z].{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 }
--- src/main/java/com/bridgelabz/userregistration/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistrationMain.java
@@ -2,6 +2,7 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistrationMain {
     public static void main(String[] args) {
-
+        User user = new User();
+        user.setFirstName("Sumit");
     }
 }
